93d8040fb841dcfe4bb61b812bd5b507a25f0683
Vivian O <kvho@umich.edu>
2024-07-11 11:52:53 -0400

    Completed up to the 7th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+    let total = 0;
+
+    for (i = min; i <= max; i++) {
+        total += i;
+    }
+
+    return total;
 
 };
 
