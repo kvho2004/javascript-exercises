e6d008377dcc81ba3a5f52d447bf8c74d067a6a9
Vivian O <kvho@umich.edu>
2024-07-11 11:33:47 -0400

    Completed the first four exercises, practicing usage of strings and arrays methods

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+    if (num < 0) return "ERROR";
+    let product = "";
+    for ( i = 0; i < num; i++ ) {
+        product += str;
+    }
 
+    return product
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(lst, ...args) {
+    return lst.filter(val => !args.includes(val))
 };
 
 // Do not edit below this line
